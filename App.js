31657dfa989f578e86ba4179f876b1c878e70468
Biswopaban Nayak <biswopabannayak@gmail.com>
2022-12-03 10:55:37 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Onboarding from './src/screen/Onboarding';
+
+const App = () => {
+
+  return (
+    <Onboarding />
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
